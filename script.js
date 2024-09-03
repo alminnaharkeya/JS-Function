b700eff3788f6c0765f8beafdd4389f6548b6c3c
alminnaharkeya <kalminnaharkeya2020@gmail.com>
2024-09-03 13:17:05 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// change HTML content
+
+function myFunction(){
+    document.getElementById("main").innerHTML = "Don't let other people write your script.";
+}
+
+// Change HTML Style-1
+
+function myStyle(){
+    document.getElementById("element").style.fontSize = '35px';
+}
+
+// Change HTML Style-2
+
+function myPower(){
+    document.getElementById("change").style.color = 'blue';
+}
+
+// Change HTML Element-1`
+
+function hideline(){
+    document.getElementById("demo").style.display = 'none';
+}
+
+// Change HTML Element-2
+
+function showline(){
+    document.getElementById("done").style.display = 'block';
+}
+
+// Change HTML Attribute
+
+function myImage(){
+    document.getElementById("img").src='image/golpo.jpg';
+}
+function yourimage(){
+    document.getElementById("img").src='image/rafin.jpg';
+}
